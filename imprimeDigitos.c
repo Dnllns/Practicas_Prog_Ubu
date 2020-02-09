ad0f3318d352428374cfcd732f5ee30ce9c82459
Daniel Alonso <dalonsobascones@gmail.com>
2020-02-09 15:03:02 +0100

    Add files via upload

---
+++ imprimeDigitos.c
@@ -0,0 +1,14 @@
+
+int main(){
+  int n;
+  printf("Escribe un número: ");
+  scanf("%d", &n);
+  while(n>0){
+    imprimeValor(n);
+    n = n/10;
+  }
+}
+void imprimeValor(int n){
+  printf("%d ", n % 10);
+}
+
---
+++ multRusa.c
@@ -0,0 +1,11 @@
+int multRusa(int mtdo, int mtdor){
+  int acu = 0;
+  while(mtdor > 0){
+    if(mtdor/2 != 0){
+      acu += mtdo;
+    }
+    mtdo *= 2;
+    mtdor /= 2;
+  }
+return acu;
+}
